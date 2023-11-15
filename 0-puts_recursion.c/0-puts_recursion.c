1c22e1e0281ac4b62f4191b512fccdd8957b3926
Scylla1994 <kaydot524@gmail.com>
2023-11-15 10:11:38 -0800

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0-puts_recursion.c/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string plus new line.
+ * @s: The string  variable to be printed.
+ * void: prints a string.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar (*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+		return;
+	}
+}
