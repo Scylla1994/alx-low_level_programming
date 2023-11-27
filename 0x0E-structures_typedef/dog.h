687ef1ce0d318599b2b7c4d84e7e8dc52d3a58ee
Scylla1994 <kaydot524@gmail.com>
2023-11-27 12:14:06 -0800

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure for a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ *
+ * Description: Structure for a dog
+ */
+
